577080e63dd2b66d8c8b36a1e40c4f8acae78434
Guillaume Paquet <paquetgp@gmail.com>
2016-10-31 00:49:43 -0400

    refactor(structure): Add a Xcode project

---
+++ mnuboSDK/mnuboSDK.h
@@ -0,0 +1,18 @@
+//
+//  mnuboSDK.h
+//
+//  Copyright (c) 2016 mnubo. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mnuboSDK.
+FOUNDATION_EXPORT double mnuboSDKVersionNumber;
+
+//! Project version string for mnuboSDK.
+FOUNDATION_EXPORT const unsigned char mnuboSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mnuboSDK/PublicHeader.h>
+
+
